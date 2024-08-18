5bc43aada02dc68d656604c7e6a3e2623ae3b666
ayoung <ayoung7777@gmail.com>
2024-08-18 20:59:01 +0900

    [CHORE] init

---
+++ src/main/java/com/fav/daengnyang/DaengnyangApplication.java
@@ -0,0 +1,13 @@
+package com.fav.daengnyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengnyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaengnyangApplication.class, args);
+	}
+
+}
