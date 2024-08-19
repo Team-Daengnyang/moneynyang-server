ef6514b3c8eac1a612fd1d85d34f1396ca5785ad
김수영 <kswim8914@gmail.com>
2024-08-19 17:54:20 +0900

    [INIT] Cashwalk domain 폴더 구조 생성

---
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/controller/CashwalkController.java
@@ -0,0 +1,4 @@
+package com.fav.daengnyang.domain.cashwalk.controller;
+
+public class CashwalkController {
+}
---
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/entity/Cashwalk.java
@@ -0,0 +1,4 @@
+package com.fav.daengnyang.domain.cashwalk.entity;
+
+public class Cashwalk {
+}
