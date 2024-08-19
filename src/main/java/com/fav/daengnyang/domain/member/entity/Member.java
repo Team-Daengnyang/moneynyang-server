aa89176ede3b3fe9fd545dfe3dc245668d4f6769
ayoung-kim <ayoung7777@gmail.com>
2024-08-19 23:21:18 +0900

    [FEAT] Member ENTITY 구현

--- src/main/java/com/fav/daengnyang/domain/member/entity/Member.java
+++ src/main/java/com/fav/daengnyang/domain/member/entity/Member.java
@@ -1,4 +1,34 @@
 package com.fav.daengnyang.domain.member.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "system_id")
+    private long systemId;
+
+    @Column(name = "member_id")
+    private long memberId;
+
+    @Column(name = "member_password")
+    private String memberPassword;
+
+    @Column(name = "member_name")
+    private String memberName;
+
+    @Column(name = "resident_id")
+    private String residentId;
+
+    @Column(name = "my_password")
+    private String myPassword;
 }
