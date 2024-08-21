d56a0d6817cf94fa54b06ea399a23e4efa184e05
choeseungbin <csb9427@naver.com>
2024-08-21 16:47:42 +0900

    [FEAT] 계좌 생성 Controller 코드 작성

--- src/main/java/com/fav/daengnyang/domain/bankbook/controller/BankbookController.java
+++ src/main/java/com/fav/daengnyang/domain/bankbook/controller/BankbookController.java
@@ -1,4 +1,24 @@
 package com.fav.daengnyang.domain.bankbook.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fav.daengnyang.domain.bankbook.service.dto.BankbookService;
+import com.fav.daengnyang.domain.bankbook.service.dto.request.BankbookRequest;
+import com.fav.daengnyang.domain.bankbook.service.dto.response.BankbookResponse;
+import com.fav.daengnyang.global.web.dto.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bankbook")
+@RequiredArgsConstructor
 public class BankbookController {
+
+    private final BankbookService bankbookService;
+
+    @PostMapping("/create")
+    public ResponseEntity<SuccessResponse<BankbookResponse>> createBankbook(@RequestBody BankbookRequest request) throws JsonProcessingException {
+        BankbookResponse response = bankbookService.createBankbook(request);
+        return ResponseEntity.ok(SuccessResponse.ok(response));
+    }
 }
