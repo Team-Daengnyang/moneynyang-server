7c770af5041e39e048c1a959f7570c2361925014
김수영 <kswim8914@gmail.com>
2024-08-23 06:50:07 +0900

    [FEAT] 특정 memberId를 가진 Bankbook의 모든 Target을 조회하는 JPQL 쿼리 구현

---
+++ src/main/java/com/fav/daengnyang/domain/target/repository/TargetRepository.java
@@ -0,0 +1,17 @@
+package com.fav.daengnyang.domain.target.repository;
+
+import com.fav.daengnyang.domain.target.entity.Target;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TargetRepository extends JpaRepository<Target, Long> {
+
+    // 특정 memberId를 가진 Bankbook의 모든 Target을 조회하는 JPQL 쿼리
+    @Query("SELECT t FROM Target t JOIN t.bankbook b WHERE b.member.memberId = :memberId")
+    List<Target> findAllByMemberId(@Param("memberId") Long memberId);
+}
