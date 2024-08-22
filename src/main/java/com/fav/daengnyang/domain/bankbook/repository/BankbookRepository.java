92973e105aef7fe55b6df72c2824c7542e00030d
choeseungbin <csb9427@naver.com>
2024-08-22 13:35:38 +0900

    [FEAT] 커스텀 색상 변경 모델 코드 작성

--- src/main/java/com/fav/daengnyang/domain/bankbook/repository/BankbookRepository.java
+++ src/main/java/com/fav/daengnyang/domain/bankbook/repository/BankbookRepository.java
@@ -3,5 +3,8 @@ package com.fav.daengnyang.domain.bankbook.repository;
 import com.fav.daengnyang.domain.bankbook.entity.Bankbook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BankbookRepository extends JpaRepository<Bankbook, Long> {
+    Optional<Bankbook> findByUserKey(String userKey);
 }
