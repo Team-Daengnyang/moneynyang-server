59b365f0e6e81ca3e2558665a8668a3e6dbe5e11
김수영 <kswim8914@gmail.com>
2024-08-23 06:49:30 +0900

    [FEAT] Response dto 구현

---
+++ src/main/java/com/fav/daengnyang/domain/target/service/dto/response/TargetResponse.java
@@ -0,0 +1,30 @@
+package com.fav.daengnyang.domain.target.service.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class TargetResponse {
+    private Long targetId;
+    private String description;
+    private int targetAmount;
+    private String targetTitle;
+    private int currentAmount;
+    private Boolean isDone;
+    private Long bankbookId;
+
+    @Builder
+    public TargetResponse (Long targetId, String description, int targetAmount, String targetTitle, int currentAmount, Boolean isDone, Long bankbookId) {
+        this.targetId = targetId;
+        this.description = description;
+        this.targetAmount = targetAmount;
+        this.targetTitle = targetTitle;
+        this.currentAmount = currentAmount;
+        this.isDone = isDone;
+        this.bankbookId = bankbookId;
+    }
+}
