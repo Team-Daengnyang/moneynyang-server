c681d63302d6e36f5eb39be18c9d6d6a941d9a9a
ayoung-kim <ayoung7777@gmail.com>
2024-08-23 23:01:50 +0900

    [FEAT] petService createPet 메소드 구현

--- src/main/java/com/fav/daengnyang/domain/pet/repository/PetRepository.java
+++ src/main/java/com/fav/daengnyang/domain/pet/repository/PetRepository.java
@@ -1,4 +1,9 @@
 package com.fav.daengnyang.domain.pet.repository;
 
-public interface PetRepository {
+import com.fav.daengnyang.domain.pet.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
 }
--- src/main/java/com/fav/daengnyang/domain/pet/service/dto/PetService.java
+++ src/main/java/com/fav/daengnyang/domain/pet/service/dto/PetService.java
@@ -1,4 +1,21 @@
 package com.fav.daengnyang.domain.pet.service.dto;
 
+import com.fav.daengnyang.domain.pet.entity.Pet;
+import com.fav.daengnyang.domain.pet.repository.PetRepository;
+import com.fav.daengnyang.domain.pet.service.dto.request.CreatedPetRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class PetService {
+
+    private final PetRepository petRepository;
+
+    public void createPet(CreatedPetRequest createdPetRequest){
+        Pet pet = Pet.createPet(createdPetRequest);
+        petRepository.save(pet);
+    }
 }
