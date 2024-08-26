21581fca1e1d986127515bf861df63510570dd1a
ayoung-kim <ayoung7777@gmail.com>
2024-08-27 00:44:40 +0900

    [FEAT] transactionHeaderRequest 구현

---
+++ src/main/java/com/fav/daengnyang/global/transaction/service/TransactionService.java
@@ -0,0 +1,14 @@
+package com.fav.daengnyang.global.transaction.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class TransactionService {
+
+    // 입금하기
+    public
+}
