8896c4194823876cb481f551bc7c98a9edc39295
Kim, Sooyoung <83462874+sootudio@users.noreply.github.com>
2024-08-26 23:22:26 +0900

    [FIX] TargetDetail 변경부분 수정 (#58)
    * [FIX] account -> accounts 로 uri 변경
    
    * [FIX] bankbookDetail -> TargetDetail로 변경되며 생긴 오류들 수정

--- src/main/java/com/fav/daengnyang/domain/account/controller/AccountController.java
+++ src/main/java/com/fav/daengnyang/domain/account/controller/AccountController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class AccountController {
 
