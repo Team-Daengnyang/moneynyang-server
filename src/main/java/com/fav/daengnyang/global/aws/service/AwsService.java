bae2513a3ebc8979af4159a41131c937e3cb895f
ayoung-kim <ayoung7777@gmail.com>
2024-08-27 19:37:02 +0900

    [FEAT] S3 구현

---
+++ src/main/java/com/fav/daengnyang/global/aws/service/AwsService.java
@@ -0,0 +1,59 @@
+package com.fav.daengnyang.global.aws.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.fav.daengnyang.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import com.fav.daengnyang.global.exception.ErrorCode;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class AwsService {
+    @Value("${aws.s3.bucket}")
+    private String name;
+
+    private final AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file, String memberId) {
+        try{
+            File fileObj = convertMultiPartFileToFile(file);
+            String originalFilename = file.getOriginalFilename();
+
+            String extension = "";
+            int dotIndex = originalFilename.lastIndexOf('.');
+            if (dotIndex > 0) {
+                extension = originalFilename.substring(dotIndex);
+            }
+
+            //memberId로 랜덤
+            String uniqueFileName = memberId+extension;
+
+            s3Client.putObject(new PutObjectRequest(name, uniqueFileName, fileObj));
+            fileObj.delete();
+            return uniqueFileName;
+
+        }catch (Exception e) {
+            log.error(e.getMessage());
+
+            throw new CustomException(ErrorCode.FAILED_CONVERT_FILE);
+        }
+    }
+
+    private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+}
--- src/main/java/com/fav/daengnyang/global/exception/ErrorCode.java
+++ src/main/java/com/fav/daengnyang/global/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     JSON_PROCESSING_ERROR("JSON 처리 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     NOT_EXIST_INSURANCE("존재하지 않는 보험 ID입니다", HttpStatus.NOT_FOUND),
     PET_NOT_FOUND("존재하지 않는 반려동물입니다.", HttpStatus.NOT_FOUND),
-    ACCOUNT_NOT_FOUND("존재하지 않는 투자 계좌입니다.", HttpStatus.NOT_FOUND),;
+    ACCOUNT_NOT_FOUND("존재하지 않는 투자 계좌입니다.", HttpStatus.NOT_FOUND),
+    FAILED_CONVERT_FILE("파일변환에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),;
 
 
     private final String message;
