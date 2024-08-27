82de384ee7060b834cdf24fd4cb86eb6d3c8c266
choeseungbin <csb9427@naver.com>
2024-08-28 00:42:00 +0900

    [FEAT] cors 에러 해결

---
+++ src/main/java/com/fav/daengnyang/global/config/WebmvcConfig.java
@@ -0,0 +1,20 @@
+package com.fav.daengnyang.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebmvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3000);
+    }
+}
