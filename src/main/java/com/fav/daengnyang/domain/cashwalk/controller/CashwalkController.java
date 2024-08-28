220dc18bba5c4af70db6cf9f4b59e321c17d7233
김수영 <kswim8914@gmail.com>
2024-08-28 23:20:53 +0900

    [FEAT] Controller 메소드 추가

--- src/main/java/com/fav/daengnyang/domain/cashwalk/controller/CashwalkController.java
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/controller/CashwalkController.java
@@ -1,17 +1,16 @@
 package com.fav.daengnyang.domain.cashwalk.controller;
 
-import com.fav.daengnyang.domain.cashwalk.entity.Cashwalk;
-import com.fav.daengnyang.domain.cashwalk.service.dto.CashwalkService;
+import com.fav.daengnyang.domain.cashwalk.service.CashwalkService;
 import com.fav.daengnyang.domain.cashwalk.service.dto.request.CashwalkRequest;
+import com.fav.daengnyang.domain.cashwalk.service.dto.response.CashwalkResponse;
 import com.fav.daengnyang.global.auth.dto.MemberPrincipal;
 import com.fav.daengnyang.global.web.dto.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/cashwalks")
@@ -31,4 +30,14 @@ public class CashwalkController {
         return SuccessResponse.created(cashwalkId);
     }
 
+    // 이 달의 작성된 일지 조회
+    @GetMapping("/month")
+    public SuccessResponse<List<CashwalkResponse>> getCashwalksForMonth(
+            @AuthenticationPrincipal MemberPrincipal memberPrincipal,
+            @RequestParam String date){
+
+        // Service 호출
+        List<CashwalkResponse> responses = cashwalkService.getCashwalksForMonth(memberPrincipal.getMemberId(), date);
+        return SuccessResponse.ok(responses);
+    }
 }
