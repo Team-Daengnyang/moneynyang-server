6802bb35f9b599ff50e5563cc90f56bbfc7705a5
김수영 <kswim8914@gmail.com>
2024-08-28 23:20:26 +0900

    [FEAT] 멤버와 날짜로 캐시워크 일지 찾는 메소드 코드 작성

--- src/main/java/com/fav/daengnyang/domain/cashwalk/repository/CashwalkRepository.java
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/repository/CashwalkRepository.java
@@ -1,9 +1,14 @@
 package com.fav.daengnyang.domain.cashwalk.repository;
 
 import com.fav.daengnyang.domain.cashwalk.entity.Cashwalk;
+import com.fav.daengnyang.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface CashwalkRepository extends JpaRepository<Cashwalk, Long> {
+    Optional<Cashwalk> findByMemberAndCreatedAt(Member member, LocalDate date);
 }
