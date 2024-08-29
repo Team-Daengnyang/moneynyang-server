1118683e2dd9589f7bbfde75f58a995582091732
김수영 <kswim8914@gmail.com>
2024-08-30 02:20:03 +0900

    [REFACTOR] deleteTarget을 TargetService에서 TargetTransferService로 이동

--- src/main/java/com/fav/daengnyang/domain/target/service/TargetService.java
+++ src/main/java/com/fav/daengnyang/domain/target/service/TargetService.java
@@ -26,9 +26,7 @@ public class TargetService {
 
     private final TargetRepository targetRepository;
     private final TargetDetailRepository targetDetailRepository;
-    private final MemberRepository memberRepository;
     private final AccountRepository accountRepository;
-    private final TargetTransferService targetTransferService; // 계좌이체 금융 API를 사용하기 위한 TargetTransferService 주입
 
     // 목표 생성 메소드
     @Transactional
@@ -84,8 +82,8 @@ public class TargetService {
         // Target 조회
         Target target = targetRepository.findById(targetId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 목표를 찾을 수 없습니다."));
-  
-      // 해당 Target에 대한 TargetDetail 리스트 조회
+
+        // 해당 Target에 대한 TargetDetail 리스트 조회
         List<TargetDetail> targetDetails = targetDetailRepository.findByTarget(target);
 
         // TargetDetail 리스트를 TargetDetailResponse로 변환하여 반환
@@ -96,37 +94,5 @@ public class TargetService {
                         .createdDate(detail.getCreatedDate())
                         .build())
                 .collect(Collectors.toList());
-    }
-
-    // 목표 삭제 메소드
-    @Transactional
-    public void deleteTarget(Long memberId, Long targetId, String userKey) throws JsonProcessingException {
-
-        // Target 조회
-        Target target = targetRepository.findById(targetId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 목표를 찾을 수 없습니다."));
-
-        // TargetDetail 리스트 조회 및 amount 합산
-        List<TargetDetail> targetDetails = targetDetailRepository.findByTarget(target);
-        int totalAmount = targetDetails.stream().mapToInt(TargetDetail::getAmount).sum();
-
-        // TargetDetail 삭제
-        targetDetailRepository.deleteAll(targetDetails);
-
-        // Member 조회
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 멤버를 찾을 수 없습니다."));
-
-        // 출금 계좌 (Target의 Bankbook)와 입금 계좌 (Member의 depositAccount) 정보 추출
-        String withdrawalAccountNo = target.getAccount().getAccountNumber();
-        String depositAccountNo = member.getDepositAccount();
-
-        // 총 합계금액을 Member의 depositAccount로 이체
-        if (totalAmount > 0) {
-            targetTransferService.callTransferApi(depositAccountNo, withdrawalAccountNo, totalAmount, userKey); // 재사용
-        }
-
-        // Target 삭제
-        targetRepository.delete(target);
     }
 }
--- src/main/java/com/fav/daengnyang/domain/target/service/TargetTransferService.java
+++ src/main/java/com/fav/daengnyang/domain/target/service/TargetTransferService.java
@@ -25,6 +25,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -72,6 +73,38 @@ public class TargetTransferService {
                 .target(target)
                 .build();
         targetDetailRepository.save(detail);
+    }
+
+    // 목표 삭제 메소드
+    @Transactional
+    public void deleteTarget(Long memberId, Long targetId, String userKey) throws JsonProcessingException {
+
+        // Target 조회
+        Target target = targetRepository.findById(targetId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 목표를 찾을 수 없습니다."));
+
+        // TargetDetail 리스트 조회 및 amount 합산
+        List<TargetDetail> targetDetails = targetDetailRepository.findByTarget(target);
+        int totalAmount = targetDetails.stream().mapToInt(TargetDetail::getAmount).sum();
+
+        // TargetDetail 삭제
+        targetDetailRepository.deleteAll(targetDetails);
+
+        // Member 조회
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 멤버를 찾을 수 없습니다."));
+
+        // 출금 계좌 (Target의 Bankbook)와 입금 계좌 (Member의 depositAccount) 정보 추출
+        String withdrawalAccountNo = target.getAccount().getAccountNumber();
+        String depositAccountNo = member.getDepositAccount();
+
+        // 총 합계금액을 Member의 depositAccount로 이체
+        if (totalAmount > 0) {
+            callTransferApi(depositAccountNo, withdrawalAccountNo, totalAmount, userKey); // 재사용
+        }
+
+        // Target 삭제
+        targetRepository.delete(target);
     }
 
     // 계좌이체 금융 API
