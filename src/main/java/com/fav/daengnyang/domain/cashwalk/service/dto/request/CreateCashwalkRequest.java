c4359a0d82e0544ee752919d80c706848ab379c6
김수영 <kswim8914@gmail.com>
2024-08-30 21:00:28 +0900

    [CHORE] request dto 이름 변경

--- src/main/java/com/fav/daengnyang/domain/cashwalk/service/dto/request/CreateCashwalkRequest.java
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/service/dto/request/CreateCashwalkRequest.java
@@ -1,23 +1,21 @@
 package com.fav.daengnyang.domain.cashwalk.service.dto.request;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class CashwalkRequest {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class CreateCashwalkRequest {
     private String content;
     private LocalDate createdAt;
     private MultipartFile image;
 
     @Builder
-    public CashwalkRequest(String content, LocalDate createdAt, MultipartFile image) {
+    public CreateCashwalkRequest(String content, LocalDate createdAt, MultipartFile image) {
         this.content = content;
         this.createdAt = createdAt;
         this.image = image;
