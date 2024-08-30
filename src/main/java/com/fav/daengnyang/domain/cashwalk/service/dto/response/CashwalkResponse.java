e1104b629cf9e353e2ee36a08cee06d9771c069e
Kim, Sooyoung <83462874+sootudio@users.noreply.github.com>
2024-08-31 01:44:28 +0900

    [FEAT] 일지 조회 시 response에 image url 추가 (#133)
    * [FEAT] 캐시워크 엔티티에 imageUrl 추가
    
    * [FEAT] Request DTO 에 image 추가
    
    * [CHORE] request dto 이름 변경
    
    * [FEAT] 일지 등록 메소드에 이미지 관련 코드 추가
    
    * [FEAT] s3에 이미지 업로드 로직 추가
    
    * [FIX] 사진을 업로드하지 않았을 때 DB에 null 값을 넣도록 수정
    
    * [FEAT] 이미지 url을 반환하도록 dto애 추가
    
    * [FEAT] 캐시워크 조회 시 이미지 url을 반환하도록 로직 추가

--- src/main/java/com/fav/daengnyang/domain/cashwalk/service/dto/response/CashwalkResponse.java
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/service/dto/response/CashwalkResponse.java
@@ -17,11 +17,13 @@ public class CashwalkResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
     private String diaryContent;
+    private String imageUrl;
 
     @Builder
-    public CashwalkResponse(Long cashwalkId, LocalDate date, String diaryContent) {
+    public CashwalkResponse(Long cashwalkId, LocalDate date, String diaryContent, String imageUrl) {
         this.cashwalkId = cashwalkId;
         this.date = date;
         this.diaryContent = diaryContent;
+        this.imageUrl = imageUrl;
     }
 }
