b93d72264f3499c25458d4bac5a04d7b94dcd9e6
김수영 <kswim8914@gmail.com>
2024-08-30 20:01:15 +0900

    [FEAT] Request DTO 에 image 추가

--- src/main/java/com/fav/daengnyang/domain/cashwalk/service/dto/request/CashwalkRequest.java
+++ src/main/java/com/fav/daengnyang/domain/cashwalk/service/dto/request/CashwalkRequest.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -13,10 +14,12 @@ import java.time.LocalDate;
 public class CashwalkRequest {
     private String content;
     private LocalDate createdAt;
+    private MultipartFile image;
 
     @Builder
-    public CashwalkRequest(String content, LocalDate createdAt) {
+    public CashwalkRequest(String content, LocalDate createdAt, MultipartFile image) {
         this.content = content;
         this.createdAt = createdAt;
+        this.image = image;
     }
 }
